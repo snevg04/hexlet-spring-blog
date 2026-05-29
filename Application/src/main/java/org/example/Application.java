1777cae5f0203ac282a078db79f5e96796a67898
snevg04 <evg.sneg.117@yandex.ru>
2026-05-29 16:30:01 +1000

    update Application

--- Application/src/main/java/org/example/Application.java
+++ Application/src/main/java/org/example/Application.java
@@ -1,11 +1,14 @@
 package org.example;
 
+import org.example.model.Post;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
