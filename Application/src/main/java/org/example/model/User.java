43f7dc4958760b3421e110835d4add125ad22688
snevg04 <evg.sneg.117@yandex.ru>
2026-05-30 22:44:01 +1000

    update

---
+++ Application/src/main/java/org/example/model/User.java
@@ -0,0 +1,32 @@
+package org.example.model;
+
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(Long id) {
+
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
